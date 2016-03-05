625746be7025c55cb6500bad4a8265a581b2f58c
Stan Lindsey <contact@stanlindsey.net>
2016-03-05 18:38:56 +0000

    Fixes authorName is undefined on bigbookcardsjs

--- app/client/templates/public/components/book_card_big.js
+++ app/client/templates/public/components/book_card_big.js
@@ -1,11 +1,14 @@
 Template.bookCardBig.helpers({
-  authorName: function () {
-    var authorName = Authors.findOne(this.authorId, {
-      fields: {
-        "firstName": 1,
-        "lastName": 1
-      }
-    })
-    return authorName.firstName + " " + authorName.lastName;
-  }
+    authorName() {
+
+        const authorName = Authors.findOne(this.authorId, {
+            fields: {
+                "firstName": 1,
+                "lastName": 1
+            }
+        })
+        if (authorName !== undefined) {
+            return `${authorName.firstName} ${authorName.lastName}`;
+        }
+    }
 });
